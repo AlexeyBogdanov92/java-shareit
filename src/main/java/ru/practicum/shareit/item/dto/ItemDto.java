2b90c18a0da8f4f91caaf5ee7081ab5d65dd5033
AlexeyBogdanov92 <lexx_7@list.ru>
2024-05-23 00:52:38 +0700

    TZ - 13

--- src/main/java/ru/practicum/shareit/item/dto/ItemDto.java
+++ src/main/java/ru/practicum/shareit/item/dto/ItemDto.java
@@ -1,7 +1,22 @@
 package ru.practicum.shareit.item.dto;
 
-/**
- * TODO Sprint add-controllers.
- */
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
+@Data
+@NoArgsConstructor
+@SuperBuilder
 public class ItemDto {
+    private long id;
+    @NotBlank(message = "Название не должно быть пустым")
+    private String name;
+    @NotBlank(message = "Описание не должно быть пустым")
+    private String description;
+    @NotNull(message = "Статус не может отсутствовать")
+    private Boolean available;
+    private Long request;
 }
